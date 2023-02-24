d4ed2958653082b97f14819ed6eaf1b99092bd04
Stephen Vaughan <stavaughan@users.noreply.github.com>
2023-02-24 14:14:57 -0600

    modified frontend/vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,37 +2,60 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig(({ mode }) => {
+export default defineConfig(({ command, mode }) => {
 	const env = loadEnv(mode, process.cwd(), '');
 
 	const pathResolve = (dir: string) => path.resolve(__dirname, '.', dir);
-
-	return {
-		plugins: [react()],
-		server: {
-			host: true,
-			proxy: {
-				'/api': {
-					target: mode === 'development'
-						? env.VITE_DOMAIN_DEV
-						: env.VITE_DOMAIN_PROD,
+	if (command === 'serve') {
+		return {
+			plugins: [react()],
+			server: {
+				host: true,
+				proxy: {
+					'/api': {
+						target: env.VITE_DOMAIN_DEV,
+					},
 				},
 			},
-		},
-		resolve: {
-			alias: {
-				'@': pathResolve('src'),
-				'~': pathResolve('node_modules'),
-				'~bootstrap': pathResolve('node_modules/bootstrap'),
+			resolve: {
+				alias: {
+					'@': pathResolve('src'),
+					'~': pathResolve('node_modules'),
+					'~bootstrap': pathResolve('node_modules/bootstrap'),
+				},
 			},
-		},
-		build: {
-			minify: false,
-		},
-		test: {
-			globals: true,
-			environment: 'jsdom',
-			setupFiles: './vitest.setup.ts'
-		},
-	};
+			build: {
+				minify: false,
+			},
+			test: {
+				globals: true,
+				environment: 'jsdom',
+				setupFiles: './vitest.setup.ts'
+			}
+		}
+	} else {
+		return {
+			plugins: [react()],
+			server: {
+				host: true,
+				proxy: {
+					'/api': {
+						target: './api',
+						changeOrigin: true,
+						rewrite: (path) => path.replace(/^\/api/, ''),
+					},
+				},
+			},
+			resolve: {
+				alias: {
+					'@': pathResolve('src'),
+					'~': pathResolve('node_modules'),
+					'~bootstrap': pathResolve('node_modules/bootstrap'),
+				},
+			},
+			build: {
+				minify: true,
+			}
+		}
+	}
 });
