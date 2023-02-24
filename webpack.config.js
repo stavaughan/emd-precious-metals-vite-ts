4371811fd53bcb46d0677eb7cc88ac4b51b1511a
Stephen Vaughan <stavaughan@users.noreply.github.com>
2023-02-24 11:52:55 -0600

    Procfile change

--- webpack.config.js
+++ webpack.config.js
@@ -3,14 +3,15 @@ const path = require('path');
 
 dotenv.config();
 
-const isProduction = process.env.NODE_ENV === 'production';
+// const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
   entry: './backend/server.ts',
   target: 'node',
   mode: process.env.NODE_ENV,
   output: {
-    filename: isProduction ? 'server.[contenthash].js' : 'server.js',
+    // filename: isProduction ? 'server.[contenthash].js' : 'server.js',
+    filename: 'server.js',
     path: path.resolve(__dirname, 'dist'),
   },
   devtool: 'inline-source-map',
